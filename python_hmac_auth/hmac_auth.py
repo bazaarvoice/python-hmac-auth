f6d5d472cea4aac8040cda182f4098c4f7f0698b
Daniils Petrovs <dpetrovs@evolutiongaming.com>
2020-03-16 12:36:04 +0200

    Formatting, fix missing encoding arguments, missing key encoding

--- python_hmac_auth/hmac_auth.py
+++ python_hmac_auth/hmac_auth.py
@@ -16,67 +16,50 @@ class HmacAuth(AuthBase):
     SIGNATURE_DELIM = '\n'
     VERSION_1 = '1'
 
-    def __init__(self, api_key, secret_key,
-                 api_key_query_param = API_KEY_QUERY_PARAM,
-                 signature_http_header = SIGNATURE_HTTP_HEADER,
-                 timestamp_http_header = TIMESTAMP_HTTP_HEADER,
-                 version_http_header = VERSION_HTTP_HEADER):
+    def __init__(self, api_key, secret_key):
         self.api_key = api_key
         self.secret_key = secret_key
-        self.api_key_query_param = api_key_query_param
-        self.signature_http_header = signature_http_header
-        self.timestamp_http_header = timestamp_http_header
-        self.version_http_header = version_http_header
 
     def __call__(self, request):
         self._encode(request)
         return request
 
     def _encode(self, request):
-        timestamp = self._get_current_timestamp()
+        timestamp = _get_current_timestamp()
         self._add_api_key(request)
-        self._add_timestamp(request, timestamp)
+        _add_timestamp(request, timestamp)
         self._add_signature(request, timestamp)
-        self._add_version(request, HmacAuth.VERSION_1)
-
-    def _get_current_timestamp(self):
-        # Return current UTC time in ISO8601 format
-        return datetime.datetime.now(dateutil.tz.tzutc()).isoformat()
+        _add_version(request, HmacAuth.VERSION_1)
 
     def _add_api_key(self, request):
         # Add the API key as a query parameter
         url = request.url
         scheme, netloc, path, query_string, fragment = urlsplit(url)
         query_params = parse_qs(query_string)
-        query_params[self.api_key_query_param] = self.api_key
+        query_params[HmacAuth.API_KEY_QUERY_PARAM] = self.api_key
         new_query_string = urlencode(query_params, doseq=True)
         new_url = urlunsplit((scheme, netloc, path, new_query_string, fragment))
         request.url = new_url
-
-    def _add_timestamp(self, request, timestamp):
-        request.headers[self.timestamp_http_header] = timestamp
-
-    def _add_version(self, request, version):
-        request.headers[self.version_http_header] = version
 
     def _add_signature(self, request, timestamp):
         method = request.method
         path = request.path_url
         content = request.body
         signature = self._sign(method, timestamp, path, content)
-        request.headers[self.signature_http_header] = signature
+        request.headers[HmacAuth.SIGNATURE_HTTP_HEADER] = signature
 
     def _sign(self, method, timestamp, path, content):
         # Build the message to sign
-        message = bytearray(method) +                       \
-                  bytearray(HmacAuth.SIGNATURE_DELIM) +     \
-                  bytearray(timestamp) +                    \
-                  bytearray(HmacAuth.SIGNATURE_DELIM) +     \
-                  bytearray(path)
+
+        message = bytearray(method, 'utf-8') + \
+                  bytearray(HmacAuth.SIGNATURE_DELIM, 'utf-8') + \
+                  bytearray(timestamp, 'utf-8') + \
+                  bytearray(HmacAuth.SIGNATURE_DELIM, 'utf-8') + \
+                  bytearray(path, 'utf-8')
 
         if content:
-            message += bytearray(HmacAuth.SIGNATURE_DELIM) + bytearray(content)
+            message += bytearray(HmacAuth.SIGNATURE_DELIM, 'utf-8') + bytearray(content, 'utf-8')
 
         # Create the signature
-        digest = hmac.new(self.secret_key, message, sha256).digest()
+        digest = hmac.new(key=bytearray(self.secret_key, 'utf-8'), msg=message, digestmod=sha256).digest()
         return base64.urlsafe_b64encode(digest).strip()
