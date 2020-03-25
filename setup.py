d920265a89b9a85e36d47bdcedeab4eba45e5d0e
Daniils Petrovs <dpetrovs@evolutiongaming.com>
2020-03-25 13:23:12 +0200

    Add more supported versions in setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,10 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=['requests', 'python-dateutil']
